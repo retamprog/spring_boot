0fca30c8524029cec10c72b8354f089e4e888d5f
retamprog <retamphy2004@gmail.com>
2025-08-18 13:11:14 +0530

    initial commit

---
+++ src/test/java/com/retam/omega/OmegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retam.omega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
